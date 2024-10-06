a4cb7f6a525b1a484c6b83a1e2dd7b26c1bbce6a
ab4io <ayalkbettesfahun@gmail.com>
2024-10-06 16:30:59 +0300

    added portfolio

--- src/data/kits/portfolio.ts
+++ src/data/kits/portfolio.ts
@@ -1,8 +1,20 @@
 import wojtech from "@/assets/images/wojtech.png";
 import ayalkbetsIcon from "@/assets/images/abio.ico";
 import yihui from "@/assets/images/yihui.png";
+import david from "@/assets/images/david.png";
 
 export const portfolioKit = [
+    {
+        title: 'David Lighthouse',
+        image : david,
+        tags: ["Portfolio", "3D"],
+        url: 'https://davidlighthouse.com/',
+        author: {
+            name: "biyacraft",
+            url: "https://biyacraft.vercel.app",
+            avatar: ayalkbetsIcon,
+          },
+    },
     {
         title: 'Wojtech',
         image : wojtech,
